0d1925f65643c5d2e231ba832ddd35e9329d0fc3
saidijongo <saidijongo@gmail.com>
2024-03-12 14:16:42 +0900

    Draw Shape

---
+++ src/damin_robot1/damin_robot1/drawshape.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class DrawShapeNode(Node):
+    
+    def __init__(self):
+        super().__init__('New_Shape')
+        self.cmd_vel_pub =self.create_publisher(Twist, '/turtle1/cmd_vel',10)
+        self.timer = self.create_timer(0, self.send_values)
+        self.get_logger().info("Shape node running..")
+
+def send_values(self):
+    msg = Twist()
+    msg.linear.x = 2.0
+    msg.angular.z =1.0
+    self.cmd_vel_pub.publish(msg)
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = DrawShapeNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+#source /opt/ros/humble/setup.bash
+#source ~/ros2_ws/install/setup.bash
+#ls /opt/ros/humble/local/lib/python3.10/dist-packages/geometry_msgs
--- src/damin_robot1/setup.py
+++ src/damin_robot1/setup.py
@@ -21,7 +21,8 @@ setup(
     entry_points={
         'console_scripts': [
             "jongo_node1 = damin_robot1.node1:main",
-            "jongo_node2 = damin_robot1.node2:main"
+            "jongo_node2 = damin_robot1.node2:main",
+            "jongo_drawshape = damin_robot1.drawshape:main"
         ],
     },
 )
